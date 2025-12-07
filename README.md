5ed7a49cc3c5bc62a9360051f95c685c68e37457
HollowAOSP <ahmedshahinx0x@gmail.com>
2025-12-07 16:01:05 -0500

    rioya: Initial device tree
    Change-Id: Id1ed9927f22824914983bf9c370528f10e416d76

---
+++ README.md
@@ -0,0 +1,9 @@
+# Android device tree for realme C21Y (rioya)
+
+```
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+```
---
+++ device.mk
@@ -0,0 +1,15 @@
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# API levels
+PRODUCT_SHIPPING_API_LEVEL := 30
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
+# Inherit the proprietary files
+$(call inherit-product, vendor/realme/rioya/rioya-vendor.mk)
