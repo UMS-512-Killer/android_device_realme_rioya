9c7e0f46c20223742558e1063c371883b96b730f
HollowAOSP <ahmedshahinx0x@gmail.com>
2026-01-07 15:04:38 -0500

    rioya: Switch to OSS camera provider
    Change-Id: I22feb0b485e2c54647760cf4c1bcfffd4cf44481

--- device.mk
+++ device.mk
@@ -85,6 +85,12 @@ PRODUCT_PACKAGES += \
     android.hardware.broadcastradio@1.0 \
     android.hardware.broadcastradio@1.0-impl
 
+# Camera
+PRODUCT_PACKAGES += \
+    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service \
+    camera.device@1.0-impl
+
 # CAS
 PRODUCT_PACKAGES += \
     android.hardware.cas@1.2-service-lazy
